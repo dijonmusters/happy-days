d0af77761f9d6e2e035709d935da079cc75f8c25
Jason Walker <jasonwalker@Jasons-MacBook-Pro-M1.local>
2022-08-10 17:44:12 +0100

    Basic styles on all entries pages

--- app/routes/entries/$id/index.tsx
+++ app/routes/entries/$id/index.tsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "@remix-run/react";
 import { Link } from "react-router-dom";
 import withAuth from "~/utils/withAuth";
 import { Entry } from "../../";
+import Navigation from "~/components/navigation";
 
 export const loader = withAuth(async ({ supabaseClient, params }) => {
   const { id } = params;
@@ -18,12 +19,28 @@ export const loader = withAuth(async ({ supabaseClient, params }) => {
 const EntryDetail = () => {
   const { entry } = useLoaderData<{ entry: Entry }>();
   return (
-    <div>
-      <Link to="edit">Edit</Link>
-      <p>{entry.date}</p>
-      <h1>{entry.title}</h1>
-      <p>{entry.content}</p>
+    <>
+    <Navigation />
+      <div className="w-full flex flex-col items-center justify-center p-2">
+        <Link to="/entries">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-12 text-gray-500 cursor-pointer hover:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </Link>
+        <div className=" w-full md:w-1/2 bg-white rounded border border-gray-200 shadow-sm m-4">
+          <div className="p-5">
+            <h5 className="mb-2 text-2xl font-bold text-blue-400">{entry.title}</h5>
+            <p className="mb-2 font-normal text-gray-700">{entry.content}</p>
+            <Link to="edit" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-slate-600 rounded hover:bg-slate-800 focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+            </svg>
+              Edit
+            </Link>
+         </div>
+        </div>
     </div>
+    </>
   );
 };
 
--- app/routes/entries/index.tsx
+++ app/routes/entries/index.tsx
@@ -15,7 +15,7 @@ export type Entry = {
 
 export const loader: LoaderFunction = withAuth(
   async ({ supabaseClient, user }) => {
-    const { data: entries } = await supabaseClient.from("entries").select("*");
+    const { data: entries } = await supabaseClient.from("entries").select("*") .order('date', { ascending: false });
     return json({ entries, user });
   }
 );
@@ -26,12 +26,35 @@ export default function EntriesList() {
   return (
     <>
       <Navigation/>
-      <Link to="new">New entry</Link>
-      {entries.map((entry) => (
-        <div key={entry.id}>
-          <Link to={`/entries/${entry.id}`}>{entry.title}</Link>
+        <div className="container grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 p-2 md:p-0">
+          <div className="md:col-start-1 md:col-span-2 lg:col-start-1 lg:col-span-2 order-2 lg:order-none">
+              {entries.map((entry) => (
+                    <Link to={`/entries/${entry.id}`} className="flex flex-col items-center bg-white hover:bg-gray-50 rounded border md:flex-row md:max-w-full mt-2 shadow-sm relative" key={entry.id}>
+                          <img className="object-cover w-full h-36 rounded-t md:h-auto md:w-24 md:rounded-none md:rounded-l" src="https://images.unsplash.com/photo-1574629173115-01ba37282238?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1076&q=80" alt=""></img>
+                          <div className="flex flex-col justify-between p-4 leading-normal">
+                            <h5 className="text-2xl font-bold text-blue-400">{entry.title}</h5>
+                            <p className="text-gray-700">{entry.content}</p>
+                          </div>
+                          <p className="absolute bottom-0 right-0 text-gray-500 text-sm m-2 hidden md:block">{entry.date}</p>
+                    </Link>
+                ))}
+          </div>
+          <div className="md:col-start-3 md:col-span-1 lg:col-start-3 lg:col-span-1 order-1 lg:order-none">
+
+            <Link to="new" className="bg-slate-600 hover:bg-gray-800 text-white text-center flex rounded-sm px-6 py-3 duration-200 transition-color mt-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
+            </svg>
+              Add New Entry
+            </Link>
+
+            <div className="hidden md:flex md:flex-col space-y-2 mt-3 p-4 w-full bg-white shadow-sm">
+              <div className="text-xl">Enjoying <span className="text-pink-400">Happy</span> <span className="text-blue-400">Days</span>?</div>
+              <div className="text-gray-500 text-sm">Our paid plans give you access to a dedicated Account Manager, unlocks premium features and much more.</div>
+              <Link to="/" className="px-4 py-2 border border-pink-400 hover:bg-pink-400 text-pink-400 hover:text-white rounded text-center">Upgrade</Link>
+            </div>
+          </div>
         </div>
-      ))}
     </>
   );
 }
